9cd700a7995f6e84549c09081d42487c85f51306
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:01 -0400

    comment

--- include/particles/Particle.h
+++ include/particles/Particle.h
@@ -1,79 +1,61 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /**
- * Particule
+ * File: Particle.h
+ * Description: This file contains the definition of the Particle class, which represents a particle in the game.
+ * Created by: Romain
+ * Created on: 06/07/2020
  */
-// Created by Romain on 06/07/2020.
 
 #ifndef DUST_PARTICLE_H
 #define DUST_PARTICLE_H
 
-#include "Particle.fwd.h"
-#include "game/Map.fwd.h"
-#include "ParticleType.h"
-#include "view/Color.h"
+#include "Particle.fwd.h"   // Forward declaration of Particle
+#include "game/Map.fwd.h"   // Forward declaration of Map
+#include "ParticleType.h"   // Include ParticleType header
+#include "view/Color.h"     // Include Color header
 
 /**
- * Particule
+ * Class: Particle
+ * Description: This class represents a particle in the game. It contains information about the particle's type, color, and state.
  */
 class Particle {
-    friend class Map;
-
 public:
     /**
-     * Constructeur
-     * @param type Type de particule
-     * @param color Couleur de la particule
+     * Constructor: Particle
+     * Description: Constructs a new Particle object with the given type and color.
+     * Parameters:
+     *  type - The type of the particle.
+     *  color - The color of the particle.
      */
     Particle(ParticleType type, Color color);
 
     /**
-     * Destructeur
+     * Destructor: ~Particle
+     * Description: Destructs the Particle object.
      */
     virtual ~Particle() = default;
 
     /**
-     * Si la particule a changé ou non pendant son update
-     * @return Changée ou non
+     * Function: hasChanged
+     * Description: Returns whether the particle has changed or not during its update.
+     * Returns:
+     *  true - If the particle has changed.
+     *  false - If the particle has not changed.
      */
     [[nodiscard]] bool hasChanged() const;
 
     /**
-     * Getter de la couleur
-     * @return Couleur
+     * Function: getColor
+     * Description: Returns a reference to the particle's color.
+     * Returns:
+     *  Color& - A reference to the particle's color.
      */
     [[nodiscard]] Color& getColor();
 
     /**
-     * Méthode à exécuter lors de l'update
+     * Function: update
+     * Description: The method to be executed during the particle's update.
      */
     virtual void update() = 0;
 
-protected:
-    /**
-     * Marque la particule comme modifiée
-     */
-    void setChanged();
-
-    /**
-     * Marque la particule comme non-modifiée
-     */
-    void clearChanged();
-
-    /**
-     * Couleur de la particule
-     */
-    Color color;
-private:
-    /**
-     * Particule modifiée ou non
-     */
-    bool stateChanged;
-
-    /**
-     * Type de particule
-     */
-    ParticleType type;
-};
-
-
-#endif //DUST_PARTICLE_H
+protected
