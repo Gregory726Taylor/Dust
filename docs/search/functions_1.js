48dcee53679c72cb505c12ea117197c251b0f638
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:06 -0400

    comment

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,15 @@
-var searchData=
-[
-  ['clearchanged',['clearChanged',['../classParticle.html#a8ef46b2bdb02c5aad640748b7b01b040',1,'Particle::clearChanged()'],['../classObservable.html#a98b488f7ef648cf1da2c8bac7f0cce26',1,'Observable::clearChanged()']]],
-  ['countobservers',['countObservers',['../classObservable.html#a86a88f7a9d862e6dd192c76eb042cff1',1,'Observable']]]
+// An array of data used for search suggestions.
+// Each element in the array is an array itself,
+// containing the name of the function, the function itself,
+// and the location of the function in the documentation.
+var searchData = [
+  // The 'clearChanged' function, which has two implementations:
+  // one in the 'Particle' class and another in the 'Observable' class.
+  ['clearchanged', [
+    ['../classParticle.html#a8ef46b2bdb02c5aad640748b7b01b040', 1, 'Particle::clearChanged()'],
+    ['../classObservable.html#a98b488f7ef648cf1da2c8bac7f0cce26', 1, 'Observable::clearChanged()']
+  ]],
+
+  // The 'countObservers' function, which is a method of the 'Observable' class.
+  ['countobservers', ['countObservers', ['../classObservable.html#a86a88f7a9d862e6dd192c76eb042cff1', 1, 'Observable']]]
 ];
