334452a7d2f71f3f483e2334208945b9164b5319
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:36 -0400

    comment

--- include/Main.h
+++ include/Main.h
@@ -1,23 +1,28 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
 /**
- * Main
+ * @file Main.h
+ * @brief The header file for the main entry point of the Dust application.
+ * @author Romain
+ * @date 2020-07-05
  */
-// Created by Romain on 05/07/2020.
 
 #ifndef DUST_MAIN_H
 #define DUST_MAIN_H
 
-#include <iostream>
+#include <iostream> // Include the iostream library for standard input and output streams.
 
+// Include the necessary header files for the game's Map, Particle, SandParticle, and ParticleType classes.
 #include "game/Map.h"
 #include "particles/Particle.h"
 #include "particles/SandParticle.h"
-
 #include "particles/ParticleType.h"
 
+// Include the necessary header files for the Window and Console classes for the game's view.
 #include "view/Window.h"
-
-#include "game/Game.h"
 #include "view/Console.h"
+
+// Include the necessary header file for the Game class for the game's logic.
+#include "game/Game.h"
 
 #endif //DUST_MAIN_H
